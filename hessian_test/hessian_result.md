034bba922b5477349a82ada3def69aef55a8bb84
HackeZ <767110505@qq.com>
2016-12-03 12:23:22 +0800

    update hessian test

---
+++ hessian_test/hessian_result.md
@@ -0,0 +1,10 @@
+```javascript
+var stringObject = {
+    color: 'red', 
+    model: 'corvette', 
+    '__type__': 'example.Car'
+};
+```
+result:
+
+> 43 0b 65 78 61 6d 70 6c 65 2e 43 61 72 92 05 63 6f 6c 6f 72 05 6d 6f 64 65 6c 60 03 72 65 64 08 63 6f 72 76 65 74 74 65
---
+++ hessian_test/hessian_test.js
@@ -0,0 +1,29 @@
+var Hessian = require('hessian-proxy');
+Long = require('long');
+
+// STRING OBJECT
+var stringObject = {
+    color: 'red', 
+    model: 'corvette', 
+    '__type__': 'example.Car'
+};
+
+write = new Hessian.Writer2()
+var stringBuffer = write.writeObject(stringObject);
+
+console.log('stringObject => ', stringObject);
+console.log('stringBuffer => ', stringBuffer.getBuffer());
+
+// NUMBER OBJECT
+var numberObject = {
+    int: 129, 
+    int32: new Long(139),
+    // int64: new double(149),
+    '__type__': 'example.Height'
+};
+
+write = new Hessian.Writer2()
+var numberBuffer = write.writeObject(numberObject);
+
+console.log('numberObject => ', numberObject);
+console.log('numberBuffer => ', numberBuffer.getBuffer());
